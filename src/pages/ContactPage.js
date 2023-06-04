94a3a2d7d15b5415e88f08f141dcaa95bb93a51d
cjiin <wldlsdl915@naver.com>
2023-06-04 23:40:48 +0900

    :lipstick: 고객센터 반응형 적용
    고객센터 반응형 적용

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,14 +1,20 @@
 import React from "react";
 import AppLayout from "../components/common/AppLayout";
 import ChatRoom from "../components/Contact/ChatRoom";
+import BottomNav from "../components/common/BottomNav";
+
+
 
 
 const ContactPage = () => {
 
   return (
+    <>
     <AppLayout>
       <ChatRoom/>
-    </AppLayout>
+      </AppLayout>
+     <BottomNav/>
+     </>
   );
 }
 
