715210d11e912cb24b32f74f3fe1282ec0baa372
cjiin <wldlsdl915@naver.com>
2023-06-06 22:02:56 +0900

    :boom: 축제 상세페이지 진행중..
    축제 상세페이지 진행중..

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import FestivalPage from './pages/FestivalPage'
 import ContactPage from './pages/ContactPage'
 import AdiminPage from './pages/AdminPage'
 import ResetPwdPage from './pages/ResetPwdPage';
+import FestivalDetailPage from './pages/FestivalDetailPage';
+
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -46,6 +48,7 @@ function App() {
           <Route path='/contact' element={<ContactPage />} />
           <Route path='/festival' element={<FestivalPage />} />
           <Route path='/admin' element={<AdiminPage />} />
+          <Route path='/festival-info' element={<FestivalDetailPage />} />
         </Routes>
       </Router>
     </>
