3e079771506cd5b4398424f0baad622f80715f06
cjiin <wldlsdl915@naver.com>
2023-06-21 11:19:51 +0900

    :white_check_mark: 상세페이지 요약, 지도 ok
    ok

--- src/pages/FestivalDetailPage.js
+++ src/pages/FestivalDetailPage.js
@@ -4,7 +4,6 @@ import AppLayout from "../components/common/AppLayout";
 import FestivalMap from "../components/Festival/Info/FestivalMap";
 import BottomNav from "../components/common/BottomNav";
 import FestivalHeader from "../components/Festival/Info/FestivalHeader";
-import FestivalCarousel from "../components/Festival/Info/FestivalCarousel";
 import FestivalSummary from "../components/Festival/Info/FestivalSummary";
 import Recommend from "../components/Festival/Info/FestivalRecommend";
 import Carousel from "../components/Festival/Info/Carousel";
