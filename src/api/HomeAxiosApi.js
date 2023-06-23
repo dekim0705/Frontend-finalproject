00dc868ed159fa06dccbe97d90166077873bd6bb
chicken-tender <ygm815@naver.com>
2023-06-23 10:55:10 +0900

    :bug: 401 에러시 catch 구문으로 넘겨주기

--- src/api/HomeAxiosApi.js
+++ src/api/HomeAxiosApi.js
@@ -4,31 +4,46 @@ const KH_DOMAIN = `${process.env.REACT_APP_API_DOMAIN}`;
 
 const HomeAxiosApi = {
   // 🍉 회원 프로필 이미지
-  pfImg : async(token) => {
-    return await axios.get(KH_DOMAIN + "/home/profile", {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + token
-      }
-    });
+  pfImg: async (token) => {
+    try {
+      return await axios.get(KH_DOMAIN + "/home/profile", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+      });
+    } catch (error) {
+      throw error;
+    }
   },
   // 🍉 전체 글 가져오기
-  allPosts : async(token) => {
-    return await axios.get(KH_DOMAIN + "/home/posts", {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + token
-      }
-    });
+  allPosts: async (token) => {
+    try {
+      return await axios.get(KH_DOMAIN + "/home/posts", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+      });
+    } catch (error) {
+      throw error;
+    }
   },
   // 🍉 특정 지역 전체 글 가져오기
-  regionAllPosts : async(city, token) => {
-    return await axios.get(`${KH_DOMAIN}/home/posts/${encodeURIComponent(city)}`, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + token
-      }
-    });
+  regionAllPosts: async (city, token) => {
+    try {
+      return await axios.get(
+        `${KH_DOMAIN}/home/posts/${encodeURIComponent(city)}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token,
+          },
+        }
+      );
+    } catch (error) {
+      throw error;
+    }
   }
 };
 
