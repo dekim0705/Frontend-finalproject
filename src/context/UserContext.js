3cde63cff7f3fb1f946b41884e8640bdf72b2621
dekim0705 <dekim0705@gmail.com>
2023-06-27 16:12:40 +0900

    :boom: UserContext에 코드 추가
    유저컨텍스트 줍줍,,

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -5,12 +5,33 @@ const UserStore = (props) => {
   const [userPfImg, setUserPfImg] = useState("");
   const [isMembership, setIsMembership] = useState("");
   const [userId, setUserId] = useState(0);
+  const [nickname, setNickname] = useState("");
+  const [userComment, setUserComment] = useState("");
+  const [postCount, setPostCount] = useState("");
+  const [replyCount, setReplyCount] = useState("");
 
   return (
-    <UserContext.Provider value = {{userPfImg, setUserPfImg, isMembership, setIsMembership, userId, setUserId}}>
+    <UserContext.Provider
+      value={{
+        userPfImg,
+        setUserPfImg,
+        isMembership,
+        setIsMembership,
+        userId,
+        setUserId,
+        nickname,
+        setNickname,
+        userComment,
+        setUserComment,
+        postCount,
+        setPostCount,
+        replyCount,
+        setReplyCount,
+      }}
+    >
       {props.children}
     </UserContext.Provider>
   );
-}
+};
 
 export default UserStore;
