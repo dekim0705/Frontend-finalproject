a18210726d75d5c86b400ef9ce4c00da82c1bd56
cjiin <wldlsdl915@naver.com>
2023-06-29 09:46:59 +0900

    :lipstick: 날짜 format 변경
    변경

--- src/components/Admin/UserManagement.js
+++ src/components/Admin/UserManagement.js
@@ -196,7 +196,7 @@ const UserManagement = () => {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
-    return `${year}. ${month} . ${day}`;
+    return `${year}-${month}-${day}`;
   };
 
   return (
