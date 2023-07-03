10d8d023f35a39430fdb9c77172bddd12d9e13fe
chicken-tender <ygm815@naver.com>
2023-07-03 14:15:48 +0900

    :zap: 이미지 여러장 업로드 기능 추가

--- src/components/PinView/PostContent2.js
+++ src/components/PinView/PostContent2.js
@@ -7,15 +7,30 @@ const StyledContainer = styled(Container)`
   line-height: 1.8em;
 `;
 
+const StyledImg = styled.img`
+  max-width: 100%;
+  height: auto;
+  margin-top: 1.5rem;
+  border-radius: 5px;
+
+  @media (max-width: 480px) {
+    min-width: 100%;
+  }
+`;
+
 const PostContent2 = ({ postData }) => {
   if (!postData) {
     return <p>데이터가 없습니다!</p>;
   }
 
+  const urls = postData.imgUrl ? postData.imgUrl.split(',') : [];
+
   return (
     <StyledContainer>
       <div dangerouslySetInnerHTML={{ __html: postData.content }} />
-      <img src={postData.imgUrl} alt="" />
+      {urls.length > 0 && urls.map((url, index) => (
+          <StyledImg key={index} src={url} alt={`첨부 이미지 ${index + 1}`} />
+        ))}
     </StyledContainer>
   );
 };
